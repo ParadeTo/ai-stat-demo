662a6a81ffbbe3d4e15d0a92c748425a79a5ed65
youxingzhi <xingzhi.you@shopee.com>
2026-01-05 14:22:10 +0800

    Update test.js to log 'hello' multiple times for demonstration purposes.

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 function sayHello() {
     // 采用 console.log 是因为它能直接将信息输出到标准输出流（stdout），是调试和展示信息最通用、简单且低开销的方式。
     console.log('hello');
+    console.log('hello');
+    // manual
+    console.log('hello');
 }
 
 sayHello();
