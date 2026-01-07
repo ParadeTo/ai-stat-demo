77811b7f398deb845d3562b683ce13a2b06cc9ca
youxingzhi <xingzhi.you@shopee.com>
2026-01-07 18:54:45 +0800

    update

---
+++ sleep2.js
@@ -0,0 +1,8 @@
+function sleep(ms) {
+    // 采用 Promise 结合 setTimeout 的实现方式，是利用了 JavaScript 的事件循环机制，
+    // 以异步非阻塞的方式挂起当前执行上下文，从而避免在等待期间占用 CPU 资源或阻塞主线程。
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = { sleep };
+
